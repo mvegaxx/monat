772fc8ba47f360f46ee4f1bacdd1df0a471a084b
al02522204 <al02522204@tecmilenio.mx>
2019-05-31 18:08:45 -0500

    primer commit a monat

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+
+// animaciones menu
+    var infomenu = $(".info-menu");
+    var logo = $(".container-logo");
+    var nav = $(".nav-menu");
+    var menubutton = $(".menu-button");
+
+//animacion index
+
+    var infohero = $(".info-hero-home");
+
+
+
+
+var tl = new TimelineLite();
+
+    tl.from("body", 3.5, { autoAlpha: 0}, "+=.5")
+    .from(infomenu, .7, { bottom: 40, autoAlpha:0 }, "-=2.5")
+    .from(logo, .7, { bottom: 40, autoAlpha: 0 }, "-=2")
+    .from(nav, .7, { bottom: 40, autoAlpha: 0 }, "-=1.5")
+    .from(menubutton, .7, { bottom: 40, autoAlpha: 0 }, "-=1.5")
+    .from(infohero, .7, { opacity: 0 }, "-=1")
+   
+
+
+
+// TweenLite.from(logo, .3, { css: { top: "-150px", delay: 2} });
+
+})
