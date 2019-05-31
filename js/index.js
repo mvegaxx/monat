2d6a5f2763cbf678fdd6f9da19b9736d5ddda272
al02522204 <al02522204@tecmilenio.mx>
2019-05-31 18:45:30 -0500

    arreglo del bug de animacion de rtransparencia en body

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
 
 var tl = new TimelineLite();
 
-    tl.from("body", 3.5, { autoAlpha: 0}, "+=.5")
+    tl.to("body", 3.5, { autoAlpha: 1}, "+=.5")
     .from(infomenu, .7, { bottom: 40, autoAlpha:0 }, "-=2.5")
     .from(logo, .7, { bottom: 40, autoAlpha: 0 }, "-=2")
     .from(nav, .7, { bottom: 40, autoAlpha: 0 }, "-=1.5")
